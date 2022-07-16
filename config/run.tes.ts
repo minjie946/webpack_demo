d9a02c5886a7f8c97ee5f7aba9fccab9b0be70d2
minjie <minjie@sj56.info>
2022-07-16 12:07:28 +0800

    feat:新增文件

---
+++ config/run.tes.ts
@@ -0,0 +1,30 @@
+/**
+ * @description 测试环境
+ * @author minjie
+ * @Date 2021-12-20 18:27
+ * @LastEditTime 2022-01-14 10:32
+ * @LastEditors minjie
+ * @copyright Copyright © 2021 Shanghai Yejia Digital Technology Co., Ltd. All rights reserved.
+ */
+import webpack from 'webpack'
+import ConfigPro from './web/webpack.pro'
+
+webpack(ConfigPro, (err:any, state:any) => {
+  if (err) {
+    console.log(err.stack || err)
+  } else if (state.hasErrors()) {
+    let err = ''
+    state.toString({
+      chunks: false,
+      colors: true
+    }).split(/\r?\n/).forEach((line:any) => {
+      err += `    ${line}\n`
+    })
+    console.warn(err)
+  } else {
+    console.log(state.toString({
+      chunks: false,
+      colors: true
+    }))
+  }
+})
